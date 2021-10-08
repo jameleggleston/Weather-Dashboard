536591f30148933810d6219951c78eee0467b803
God of War <jamel.eggleston@gmail.com>
2021-10-07 23:21:08 -0400

    created js variables

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
-// start here
+// defined variables for current weather div 
+var searchButton = document.querySelector("#search-button");
+var cityInput = document.querySelector("#city-input");
+var currentWeather = document.querySelector("#current-weather");
+var currentTemp = document.querySelector("#current-temp");
+var weatherIcon = document.querySelector("#weather-icon");
+var currenthumidity = document.querySelector("#current-humidity");
+var currentWindSpeed = document.querySelector("#current-wind");
+var currentUV = document.querySelector("#current-uv");
+var cityNamesList = document.querySelector(".city-name-list");
+var clearBtn = document.querySelector("#clear-button");
+var uvBlock = document.querySelector(".Uv-block");
